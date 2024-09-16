1001b2c10d5f228309d9c80c3ffdb26adc8cb24d
1331244 <1@vvooz.pro>
2024-09-16 10:39:22 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://ssoo.lol/'
+window.settings = {
+  // 站点名称
+  title: 'ZKEOS加速',
+  // 站点描述
+  description: '速度飞快，价格亲民',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
